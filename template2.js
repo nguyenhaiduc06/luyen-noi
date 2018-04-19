7267dc07664224b15437efe3c1d5b970ccca3f66
nguyenhaiduc06 <nguyenhaiduc06@gmail.com>
2018-04-19 19:46:50 +0700

    scripts for steps progressbar, questiosn, modal

---
+++ template2.js
@@ -0,0 +1,64 @@
+window.onload = function() {
+    let questionCount = 0;
+    let questions = ['lung linh', 'lấp ló', 'no nê', 'lạ lẫm', 'nôn nóng'];
+    const button = document.querySelector('#button');
+    const steps = document.querySelectorAll('li');
+    const next = document.querySelector('.next');
+    const back = document.querySelector('.back');
+    const text = document.querySelector('.text');
+    const modal = document.querySelector('#simpleModal');
+    const closeBtn = document.querySelector('.closeBtn');
+
+    button.addEventListener('click', function() {
+        button.classList.toggle('fa-microphone');
+        button.classList.toggle('fa-spinner');
+        button.classList.toggle('fa-pulse');
+    });
+
+    next.addEventListener('click', function() {
+        if (questionCount < 4) {
+            nextQuestion();
+        } else {
+            openModal();
+        }
+    });
+    back.addEventListener('click', function() {
+        if (questionCount > 0) {
+            preQuestion();
+        }
+    });
+
+    function nextQuestion() {
+        steps[questionCount].classList.toggle('active');
+        questionCount++;
+        steps[questionCount].classList.toggle('active');
+        text.innerHTML = questions[questionCount];
+    }
+
+    function preQuestion() {
+        steps[questionCount].classList.toggle('active');
+        questionCount--;
+        steps[questionCount].classList.toggle('active');
+        text.innerHTML = questions[questionCount];
+    }
+
+    steps[questionCount].classList.toggle('active');
+    text.innerHTML = questions[questionCount];
+
+    closeBtn.addEventListener('click', closeModal);
+    window.addEventListener('click', outsideClick);
+
+    function openModal() {
+        modal.style.display = 'block';
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+    }
+
+    function outsideClick(e) {
+        if (e.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+};
